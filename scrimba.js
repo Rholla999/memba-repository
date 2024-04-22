7cbc0685bca011a60e9c2c9b4eab3a3443dd93f3
Rholla999 <104360361+Rholla999@users.noreply.github.com>
2024-04-22 13:15:04 +0100

    Add files via upload

--- scrimba.js
+++ scrimba.js
@@ -1,21 +1,7 @@
-let incBtn = document.getElementById('increment-btn');
-let countUp = document.getElementById('count-el');
-let saveBtn = document.getElementById('save-btn');
-let preEnt = document.getElementById('pre');
+let btn = document.getElementById('p-btn');
+let errorMsg = document.getElementById('error');
 
-incBtn.style.textTransform = 'upperCase';
-
-let count = 0;
-
-incBtn.addEventListener('click', () => {
-  count++;
-  countUp.textContent = count;
-});
-
-saveBtn.addEventListener('click', () => {
-  let countStr = count + ' - ';
-  preEnt.textContent += countStr;
-
-  countUp.textContent = 0;
-  count = 0;
+btn.addEventListener('click', () => {
+  errorMsg.textContent = 'Out of Stock';
+  
 });
